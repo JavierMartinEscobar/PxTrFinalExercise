954a425057d1bf68f59f7bfa2591f9c5fd437dbf
JavierMartinEscobar <javier.martinescobar@plexus.es>
2023-08-24 12:55:00 +0200

    Implement product-service connection to eureka server

---
+++ product-service/src/main/java/org/Plexus/productservice/ProductServiceApplication.java
@@ -0,0 +1,18 @@
+package org.Plexus.productservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Import;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+@EnableDiscoveryClient
+public class ProductServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductServiceApplication.class, args);
+	}
+
+}
