f182eaaf2ec30bdf1df5c60d8700a1fa37254d95
María Fernández de Córdova Cascales <maria.fernandezdecordovacascales@plexus.es>
2023-08-24 12:59:10 +0200

    Add microservice order-service

---
+++ order-service/order-service/src/main/java/org/plexus/orderservice/DataSourceConfig.java
@@ -0,0 +1,20 @@
+package org.plexus.orderservice;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+    @Bean
+    public DataSource datasource() {
+        return DataSourceBuilder.create()
+                .driverClassName("com.mysql.cj.jdbc.Driver")
+                .url("jdbc:mysql://localhost:3306/order_db")
+                .username("order")
+                .password("order_123")
+                .build();
+    }
+}
---
+++ order-service/order-service/src/main/java/org/plexus/orderservice/OrderServiceApplication.java
@@ -0,0 +1,17 @@
+package org.plexus.orderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableAutoConfiguration
+public class OrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderServiceApplication.class, args);
+	}
+}
