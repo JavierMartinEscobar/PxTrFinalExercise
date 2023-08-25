1080f9231f27d86abdffef2c47d9ab4fba9c2c07
PLEXUS\alvaro.gonzalezmadro <alvaro.gonzalezmadronal@plexus.es>
2023-08-25 09:18:43 +0200

    inventory2.0

--- inventory-service/src/main/java/org/plexus/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/org/plexus/inventoryservice/InventoryServiceApplication.java
@@ -5,8 +5,9 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+
 @EnableAutoConfiguration
+@SpringBootApplication
 @EnableDiscoveryClient
 public class InventoryServiceApplication {
 
