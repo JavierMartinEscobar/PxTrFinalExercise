1d18a638a74135e663313c5e3f2191af1d50deb4
JavierMartinEscobar <javier.martinescobar@plexus.es>
2023-08-28 13:56:38 +0200

    Create gateway-service project

---
+++ gateway-service/src/main/java/org/Plexus/gatewayservice/GatewayServiceApplication.java
@@ -0,0 +1,17 @@
+package org.Plexus.gatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableAutoConfiguration
+@EnableDiscoveryClient
+@SpringBootApplication
+public class GatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayServiceApplication.class, args);
+	}
+
+}
