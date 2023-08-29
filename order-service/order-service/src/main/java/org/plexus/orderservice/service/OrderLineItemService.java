3aa4816f5f6146126e3e8caabe905e0b052bd9c0
María Fernández de Córdova Cascales <maria.fernandezdecordovacascales@plexus.es>
2023-08-29 14:40:51 +0200

    Update configuration

--- order-service/order-service/src/main/java/org/plexus/orderservice/service/OrderLineItemService.java
+++ order-service/order-service/src/main/java/org/plexus/orderservice/service/OrderLineItemService.java
@@ -1,17 +1,106 @@
 package org.plexus.orderservice.service;
 
+import org.modelmapper.ModelMapper;
 import org.plexus.orderservice.dto.OrderLineItemDTO;
+import org.plexus.orderservice.exception.BlogAppException;
+import org.plexus.orderservice.exception.ResourceNotFoundException;
+import org.plexus.orderservice.model.Order;
+import org.plexus.orderservice.model.OrderLineItem;
+import org.plexus.orderservice.repository.OrderLineItemRepository;
+import org.plexus.orderservice.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
-public interface OrderLineItemService {
-    public OrderLineItemDTO createOrderLineItem(long orderId, OrderLineItemDTO orderLineItemDTO);
 
-    public List<OrderLineItemDTO> getOrderLineItemByOrderId(long orderId);
+@Service
+public class OrderLineItemService {
+    @Autowired
+    private ModelMapper modelMapper;
 
-    public OrderLineItemDTO getOrderLineItemById(long orderId,Long orderLineItemId);
+    @Autowired
+    private OrderLineItemRepository orderLineItemRepository;
 
-    public OrderLineItemDTO updateOrderLineItem(long orderId, long orderLineItemId, OrderLineItemDTO requestFromComment);
+    @Autowired
+    private OrderRepository orderRepository;
 
-    public void deleteOrderLineItem(long orderId, long orderLineItemId);
+    public OrderLineItemDTO createOrderLineItem(long orderId, OrderLineItemDTO orderLineItemDTO) {
+        OrderLineItem orderLineItem = mapearEntidad(orderLineItemDTO);
+        Order order = orderRepository.findById(orderId)
+                .orElseThrow(() -> new ResourceNotFoundException("Order", "id", orderId));
+
+        orderLineItem.setOrder(order);
+        order.getOrderLineItem().add(orderLineItem);
+        order.setPrice(order.getPrice() + (orderLineItem.getPrice() * orderLineItem.getQuantity()));
+        OrderLineItem newOrderLineItem = orderLineItemRepository.save(orderLineItem);
+        return mapearDTO(newOrderLineItem);
+    }
+
+    public List<OrderLineItemDTO> getOrderLineItemByOrderId(long orderId) {
+        List<OrderLineItem> orderLineItems = orderLineItemRepository.findByOrderId(orderId);
+        return orderLineItems.stream().map(orderLineItem -> mapearDTO(orderLineItem)).collect(Collectors.toList());
+    }
+
+    public OrderLineItemDTO getOrderLineItemById(long orderId, Long orderLineItemId) {
+        Order order = orderRepository.findById(orderId)
+                .orElseThrow(() -> new ResourceNotFoundException("Order", "id", orderId));
+
+        OrderLineItem orderLineItem = orderLineItemRepository.findById(orderLineItemId)
+                .orElseThrow(() -> new ResourceNotFoundException("Order line item", "id", orderLineItemId));
+
+        if(!orderLineItem.getOrder().getId().equals(order.getId())) {
+            throw new BlogAppException(HttpStatus.BAD_REQUEST, "The order line item does not belong to the order");
+        }
+
+        return mapearDTO(orderLineItem);
+    }
+
+    public OrderLineItemDTO updateOrderLineItem(long orderId, long orderLineItemId, OrderLineItemDTO requestFromComment) {
+        Order order = orderRepository.findById(orderId)
+                .orElseThrow(() -> new ResourceNotFoundException("Order", "id", orderId));
+
+        OrderLineItem orderLineItem = orderLineItemRepository.findById(orderLineItemId)
+                .orElseThrow(() -> new ResourceNotFoundException("Order line item", "id", orderLineItemId));
+
+        if(!orderLineItem.getOrder().getId().equals(order.getId())) {
+            throw new BlogAppException(HttpStatus.BAD_REQUEST, "The order line item does not belong to the order");
+        }
+
+        orderLineItem.setSkuCode(requestFromComment.getSkuCode());
+        orderLineItem.setPrice(requestFromComment.getPrice());
+        orderLineItem.setQuantity(requestFromComment.getQuantity());
+
+        OrderLineItem updateOrderLineItem = orderLineItemRepository.save(orderLineItem);
+        return mapearDTO(updateOrderLineItem);
+    }
+
+    public void deleteOrderLineItem(long orderId, long orderLineItemId) {
+        Order order = orderRepository.findById(orderId)
+                .orElseThrow(() -> new ResourceNotFoundException("Order", "id", orderId));
+
+        OrderLineItem orderLineItem = orderLineItemRepository.findById(orderLineItemId)
+                .orElseThrow(() -> new ResourceNotFoundException("Order line item", "id", orderLineItemId));
+
+        if(!orderLineItem.getOrder().getId().equals(order.getId())) {
+            throw new BlogAppException(HttpStatus.BAD_REQUEST, "The order line item does not belong to the order");
+        }
+
+        order.setPrice(order.getPrice() - (orderLineItem.getQuantity() * orderLineItem.getPrice()));
+        orderLineItemRepository.delete(orderLineItem);
+    }
+
+    // Convierte Entidad a DTO
+    private OrderLineItemDTO mapearDTO(OrderLineItem orderLineItem) {
+        OrderLineItemDTO orderLineItemDTO = modelMapper.map(orderLineItem, OrderLineItemDTO.class);
+        return orderLineItemDTO;
+    }
+
+    // Convierte de DTO a Entidad
+    private OrderLineItem mapearEntidad(OrderLineItemDTO orderLineItemDTO) {
+        OrderLineItem orderLineItem = modelMapper.map(orderLineItemDTO, OrderLineItem.class);
+        return orderLineItem;
+    }
 }
--- order-service/order-service/src/main/java/org/plexus/orderservice/service/OrderService.java
+++ order-service/order-service/src/main/java/org/plexus/orderservice/service/OrderService.java
@@ -1,17 +1,67 @@
 package org.plexus.orderservice.service;
 
+import org.modelmapper.ModelMapper;
 import org.plexus.orderservice.dto.OrderDTO;
+import org.plexus.orderservice.exception.ResourceNotFoundException;
+import org.plexus.orderservice.model.Order;
+import org.plexus.orderservice.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
-public interface OrderService {
-    public OrderDTO createOrder(OrderDTO orderDTO);
+@Service
+public class OrderService {
+    @Autowired
+    private ModelMapper modelMapper;
 
-    public List<OrderDTO> getAllOrders();
+    @Autowired
+    private OrderRepository orderRepository;
 
-    public OrderDTO getOrderById(long id);
+    public OrderDTO createOrder(OrderDTO orderDTO) {
+        Order order = mapearEntidad(orderDTO);
 
-    public OrderDTO updateOrder(OrderDTO orderDTO,long id);
+        Order newOrder = orderRepository.save(order);
 
-    public void deleteOrder(long id);
+        OrderDTO orderResponse = mapearDTO(newOrder);
+        return orderResponse;
+    }
+
+    public List<OrderDTO> getAllOrders() {
+        List<Order> orders = orderRepository.findAll();
+        return orders.stream().map(order -> mapearDTO(order)).collect(Collectors.toList());
+    }
+
+    public OrderDTO getOrderById(long id) {
+        Order order = orderRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Order", "id", id));
+        return mapearDTO(order);
+    }
+
+    public OrderDTO updateOrder(OrderDTO orderDTO, long id) {
+        Order order = orderRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Order", "id", id));
+
+        order.setOrderNumber(orderDTO.getOrderNumber());
+        order.setPrice(orderDTO.getPrice());
+
+        Order updateOrder = orderRepository.save(order);
+        return mapearDTO(updateOrder);
+    }
+
+    public void deleteOrder(long id) {
+        Order order = orderRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Order", "id", id));
+        orderRepository.delete(order);
+    }
+
+    // Convierte Entidad a DTO
+    private OrderDTO mapearDTO(Order publicacion) {
+        OrderDTO publicacionDTO = modelMapper.map(publicacion, OrderDTO.class);
+        return publicacionDTO;
+    }
+
+    // Convierte de DTO a Entidad
+    private Order mapearEntidad(OrderDTO publicacionDTO) {
+        Order publicacion = modelMapper.map(publicacionDTO, Order.class);
+        return publicacion;
+    }
 }
